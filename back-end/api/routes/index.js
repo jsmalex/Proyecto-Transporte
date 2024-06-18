ed484e373cbd05f60e0e4dc1cc18f714ea920186
reboot <flaviomelian2000@gmail.com>
2024-06-18 12:54:13 +0100

    detalles arreglados v.2

--- back-end/api/routes/index.js
+++ back-end/api/routes/index.js
@@ -1,7 +1,7 @@
 // Importamos el módulo de enrutamiento de Express y creamos un enrutador.
 const router = require("express").Router();
 
-// Usamos el enrutador para manejar todas las peticiones dirigidas a '/cliente' usando el enrutador definido en 'cliente.router'.
+// Usamos el enrutador general para manejar todas las peticiones dirigidas a las direciones definidas para el resto de enrutadores.
 router.use("/client", require("./client.router"));
 router.use("/order", require("./order.router"));
 router.use("/vehicle", require("./vehicle.router"));
